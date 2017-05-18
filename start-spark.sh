56c70dbbeb06a06819fa9f653599d633d7917f5c
Santosh Kumar Addanki <santosh.kumar.addanki@sap.com>
2017-05-18 16:08:34 -0700

    Update start-spark.sh

--- start-spark.sh
+++ start-spark.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Use the spark-master-controller.yaml file to create a replication controller running the Spark Master service
-kubectl --namespace=jupyterhub create -f spark-master-controller.yaml
+kubectl --namespace=jupyterhub create -f spark/spark-master-controller.yaml
 
 # create a logical service endpoint that Spark workers can use to access the Master pod:
-kubectl --namespace=jupyterhub create -f spark-master-service.yaml
+kubectl --namespace=jupyterhub create -f spark/spark-master-service.yaml
 
 # Deploy the proxy controller 
-kubectl --namespace=jupyterhub create -f spark-ui-proxy-controller.yaml
+kubectl --namespace=jupyterhub create -f spark/spark-ui-proxy-controller.yaml
 
 # Use the spark-worker-controller.yaml file to create a replication controller that manages the worker pods.
-kubectl --namespace=jupyterhub create -f spark-worker-controller.yaml
+kubectl --namespace=jupyterhub create -f spark/spark-worker-controller.yaml
 
