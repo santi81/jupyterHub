39b56873c1392a52ec60afd0400edbcf80a2187d
Santosh Kumar Addanki <santosh.kumar.addanki@sap.com>
2017-05-18 14:49:21 -0700

    Update make-disk.bash

--- make-disk.bash
+++ make-disk.bash
@@ -15,20 +15,20 @@ NAME="$1"
 SIZE="$2"
 disk_name="${NAME}"
 
-gcloud compute instances create provisioner-01 --zone us-central1-c	
-gcloud compute disks create ${disk_name} --zone us-central1-c --size ${SIZE} --type pd-ssd
-gcloud compute instances attach-disk provisioner-01 --disk ${disk_name} --zone us-central1-c
+gcloud compute instances create provisioner-01 	
+gcloud compute disks create ${disk_name}  --size ${SIZE} --type pd-ssd
+gcloud compute instances attach-disk provisioner-01 --disk ${disk_name} 
 
 devpath="/dev"
-device="$(gcloud compute ssh provisioner-01 --zone us-central1-c --command "sudo lsblk --output name | tail -1")"
+device="$(gcloud compute ssh provisioner-01 --command "sudo lsblk --output name | tail -1")"
 blkdev=${devpath}/${device}
 echo $blkdev
 mntpath="/mnt/disks/${disk_name}"
 echo $mntpath
 
-gcloud compute ssh provisioner-01 --zone us-central1-c --command "sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard ${blkdev}"
-gcloud compute ssh provisioner-01 --zone us-central1-c --command "sudo mkdir -p ${mntpath}"
-gcloud compute ssh provisioner-01 --zone us-central1-c --command "sudo mount -o discard,defaults ${blkdev} ${mntpath}"
+gcloud compute ssh provisioner-01  --command "sudo mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard ${blkdev}"
+gcloud compute ssh provisioner-01  --command "sudo mkdir -p ${mntpath}"
+gcloud compute ssh provisioner-01  --command "sudo mount -o discard,defaults ${blkdev} ${mntpath}"
 
 echo "New disk '${disk_name}' mounted at ${mntpath}"
 echo "After running the course-specific data retrieval script, run:"
